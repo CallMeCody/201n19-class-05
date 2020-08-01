04b73bb0bda5ef221eb9f5e9ed0d5de582781449
Cody Carpenter <cody_carpenter2222@yahoo.com>
2020-08-01 15:25:19 -0700

    problem 3 completed

--- starter-code/app.js
+++ starter-code/app.js
@@ -54,11 +54,31 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var theSum = sum(a, b)
+  var newSum = theSum[0];
+  // console.log(newSum);
+  var sumTotal = sum(newSum, c);
+  var newSumTotal = sumTotal[0];
+  // console.log(newSumTotal);
 
+
+  var theProduct = multiply(a, b)
+  var newProduct = theProduct[0];
+  // console.log(newProduct);
+  var multiplyTotal = multiply(newProduct, c);
+  var newMultiplyTotal = multiplyTotal[0];
+  // console.log(newMultiplyTotal);
+
+  var sumString = `${a} and ${b} and ${c} sum to ${newSumTotal}.`
+  var multiplyString = `The product of ${a} and ${b} and ${c} is ${newMultiplyTotal}.`
+
+  // console.log(newSumTotal, newMultiplyTotal, sumString, multiplyString);
+
+  return [newSumTotal, newMultiplyTotal, sumString, multiplyString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -122,7 +142,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
